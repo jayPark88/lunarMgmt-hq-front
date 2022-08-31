0c526b60c306f86032eb4fc8e990c43d215f2566
jayPark <jaypark8282@gmail.com>
2022-08-31 22:06:50 +0900

    🎇add: HQ-ADMIN FRONT사이드바 추가(레이아웃 수정 더 해야 됨..)
    🎇add: HQ-ADMIN FRONT사이드바 추가(레이아웃 수정 더 해야 됨..)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,27 @@
 <template>
-  <div>
-    <Navbar/>
-    <nuxt/>
-  </div>
+  <v-app>
+    <section id="main">
+      <MiscCombined />
+      <div class="content_wrap">
+        <Navbar />
+        <v-main>
+          <v-container class="main-container">
+            <Nuxt keep-alive :max="300" />
+          </v-container>
+        </v-main>
+      </div>
+    </section>
+  </v-app>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar'
+import MiscCombined from '~/components/MiscCombined'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    MiscCombined
   }
 }
 </script>
