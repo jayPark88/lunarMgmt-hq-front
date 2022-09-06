da3d6801b85d39c879e81b9947fed1ac5880d073
jayPark <jaypark8282@gmail.com>
2022-09-06 23:21:58 +0900

    🧹clean: code clean
    🧹clean: code clean

--- components/setting/auth/AuthUserViewer.vue
+++ components/setting/auth/AuthUserViewer.vue
@@ -50,128 +50,129 @@
 </template>
   
   <script>
-  import Pagination from '~/components/common/Pagination.vue'
-  import SearchUser from '~/components/searchgrid/SearchUser.vue'
-  
-  export default {
-    components: { Pagination, SearchUser },    
-    props: {
-      auth: {
-        type: Object,
-        default: () => {},
-      },
+import Pagination from '~/components/common/Pagination.vue'
+import SearchUser from '~/components/searchgrid/SearchUser.vue'
+
+export default {
+  components: { Pagination, SearchUser },
+  props: {
+    auth: {
+      type: Object,
+      default: () => { },
     },
-    data() {
-      return {
-        deleteModalTrigger: false,
-        pagination: {
-          page: 1,
-          size: 15,
-          totalElements: 0, // 총 검색된 item 갯수
-          totalPages: 0, // 총 페이지 갯수
-          numberOfElements: 0, // 현재 화면에 표시될 row 갯수
+  },
+  data() {
+    return {
+      deleteModalTrigger: false,
+      pagination: {
+        page: 1,
+        size: 15,
+        totalElements: 0, // 총 검색된 item 갯수
+        totalPages: 0, // 총 페이지 갯수
+        numberOfElements: 0, // 현재 화면에 표시될 row 갯수
+      },
+      authUserheader: [
+        {
+          text: '이름',
+          align: 'start',
+          sortable: false,
+          value: 'adminUserNm',
         },
-        authUserheader: [
-          {
-            text: '이름',
-            align: 'start',
-            sortable: false,
-            value: 'adminUserNm',
-          },
-          {
-            text: '아이디',
-            align: 'false',
-            value: 'adminUserId',
-            sortable: false,
-          },
-          {
-            text: '담당업무',
-            align: 'false',
-            sortable: false,
-            value: 'position',
-          },
-        ],
-        userList: [],
-        authUserList: [],
-        selectedAuthUserList: [],
-        dialog: false,
-      }
+        {
+          text: '아이디',
+          align: 'false',
+          value: 'adminUserId',
+          sortable: false,
+        },
+        {
+          text: '담당업무',
+          align: 'false',
+          sortable: false,
+          value: 'position',
+        },
+      ],
+      userList: [],
+      authUserList: [],
+      selectedAuthUserList: [],
+      dialog: false,
+    }
+  },
+  async fetch() {
+    this.initData()
+    if (this.auth.authSeq) {
+      this.authUserList = await this.$axios.$get(
+        `/lunar/admin/setting/auth/users/${this.auth.authSeq}`,
+      )
+    }
+  },
+  computed: {
+    headers() {
+      return [
+        { sortable: false, text: '번호', value: 'number' },
+        { sortable: false, text: '이름', value: 'adminUserNm' },
+        { sortable: false, text: 'ID', value: 'adminUserId' },
+        { sortable: false, text: '권한', value: 'authNm' },
+        { sortable: false, text: '가입일자', value: 'createDatetime' },
+      ]
     },
-    async fetch() {
-      this.initData()
-      if (this.auth.authSeq) {
-        this.authUserList = await this.$axios.$get(
-          `/lunar/admin/setting/auth/users/${this.auth.authSeq}`,
-        )
-      }
+  },
+  watch: {
+    auth() {
+      this.$fetch()
     },
-    computed: {
-      headers() {
-        return [
-          { sortable: false, text: '번호', value: 'number' },
-          { sortable: false, text: '이름', value: 'adminUserNm' },
-          { sortable: false, text: 'ID', value: 'adminUserId' },
-          { sortable: false, text: '권한', value: 'authNm' },
-          { sortable: false, text: '가입일자', value: 'createDatetime' },
-        ]
-      },
+  },
+  methods: {
+    onUserSerachDialog() {
+      if (this.$refs.searchUserRef) this.$refs.searchUserRef.init()
+      this.dialog = true
     },
-    watch: {
-      auth() {
-        this.$fetch()
-      },
+
+    createUserSeqList(userCheckedList) {
+      return userCheckedList.map((user) => user.adminUserSeq)
     },
-    methods: {
-      onUserSerachDialog() {
-        if (this.$refs.searchUserRef) this.$refs.searchUserRef.init()
-        this.dialog = true
-      },
-  
-      createUserSeqList(userCheckedList) {
-        return userCheckedList.map((user) => user.adminUserSeq)
-      },
-      push() {
-        const userSeqList = this.createUserSeqList(this.selectedAuthUserList)
-        this.$axios
-          .post(`/lunar/admin/setting/auth/users/${this.auth.authSeq}`, userSeqList)
-          .then(() => {
-            this.$fetch()
-            this.dialog = false
-          })
-          .catch((error) => {
-            alert(error.response.data.message)
-          })
-      },
-      
-      deleteUserAuth() {
-          const userSeqList = this.createUserSeqList(this.selectedAuthUserList)         
-          this.$axios
-            .delete(`/lunar/admin/setting/auth/users/${this.auth.authSeq}`,{data:userSeqList})
-            .then(() => {
-              this.$fetch()
-            })
-            .catch((error) => {
-              alert(error.response.data.message)
-            })
-      },
-      setUserList(data) {
-        this.userList = data.contents
-        this.pagination = Object.assign(this.pagination, data.pageInfo)
-      },
-      initData() {
-        this.selectedAuthUserList = []
-      },
-      boolToString(b) {
-        return b ? '사용' : '미사용'
-      },
+    push() {
+      const userSeqList = this.createUserSeqList(this.selectedAuthUserList)
+      this.$axios
+        .post(`/lunar/admin/setting/auth/users/${this.auth.authSeq}`, userSeqList)
+        .then(() => {
+          this.$fetch()
+          this.dialog = false
+        })
+        .catch((error) => {
+          alert(error.response.data.message)
+        })
     },
-  }
-  </script>
+
+    deleteUserAuth() {
+      const userSeqList = this.createUserSeqList(this.selectedAuthUserList)
+      this.$axios
+        .delete(`/lunar/admin/setting/auth/users/${this.auth.authSeq}`, { data: userSeqList })
+        .then(() => {
+          this.$fetch()
+        })
+        .catch((error) => {
+          alert(error.response.data.message)
+        })
+    },
+    setUserList(data) {
+      this.userList = data.contents
+      this.pagination = Object.assign(this.pagination, data.pageInfo)
+    },
+    initData() {
+      this.selectedAuthUserList = []
+    },
+    boolToString(b) {
+      return b ? '사용' : '미사용'
+    },
+  },
+}
+</script>
   
   <style lang="scss" scoped>
   .user-list__vcard {
     padding: 4%;
   }
+  
   .check-table-style {
     padding: 0;
   }
